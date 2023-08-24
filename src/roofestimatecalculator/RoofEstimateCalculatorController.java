b8463060d8be5118a35d15440925c631f6aae5c2
Tyler Zytkowski <87623562+tzytkowski@users.noreply.github.com>
2023-08-23 21:02:18 -0400

    Initial commit
    I know Java FX isn't too relevant but just laying out idea

---
+++ src/roofestimatecalculator/RoofEstimateCalculatorController.java
@@ -0,0 +1,44 @@
+package roofestimatecalculator;
+
+
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RoofEstimateCalculatorController {
+
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    private Circle radius;
+
+    @FXML
+    private Line radiusLine;
+
+    @FXML
+    private Label roofLengthInput;
+
+    @FXML
+    private Label roofPitchLabel;
+
+    @FXML
+    private TextField roofWidthInput;
+
+    @FXML
+    private Label roofWidthLabel;
+
+    @FXML
+    private Label storiesLabel;
+    @FXML
+    private Button submitButton;
+   
+
+
+}
+
+
